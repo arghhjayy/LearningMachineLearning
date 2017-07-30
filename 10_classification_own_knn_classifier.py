2457836749b7e5dec5c536b78ab1404dae5a7108
Rupanshu Janbandhu <arghhjayy@gmail.com>
2017-07-30 18:52:11 +0530

    Add comments, previous algos revisit

--- 10_classification_own_knn_classifier.py
+++ 10_classification_own_knn_classifier.py
@@ -5,31 +5,52 @@ import matplotlib.pyplot as plt
 from matplotlib import  style
 from collections import Counter
 
+#Use this style to plot the graph
 style.use('fivethirtyeight')
 
+#Dataset has two groups:
+#group k and group r
+#Plotting the points [1, 2], [2, 3], [3, 1] and [6, 5], [7, 7] and [8, 6] on a graph
+#will make it easier to understand the groups
 dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
 
+#Which group does this point belong to?
 new_features = [5, 7]
 
 def k_nearest_neighbors(data, predict_for, k = 3):
+    '''
+
+    Given training data(data),
+    find the group to which the point (predict_for) belongs to
+
+    '''
     if len(data) >= k:
         warnings.warn('k is set to a value less than the total voting groups, boi')
 
+    #All of them distances
     distances = []
 
+    #For each group i. e. for k and r
     for group in data:
+        #For each of their points
         for features in data[group]:
+            #Calculate the distance of the point belonging to group x(k or r) from the 'predict_for' point
             euclidean_distance = np.linalg.norm(np.array(features) - np.array((predict_for)))
+            #Add this distance to the list of distances along with that group signature(k or r)
             distances.append([euclidean_distance, group])
 
+    #Find the group of the first k points which are closest to the 'predict_for' point
     votes = [i[1] for i in sorted(distances)[:k]]
+    #Calculate which group had the maximum of the votes
     vote_result = Counter(votes).most_common(1)[0][0]
 
     return vote_result
 
+#Calculate group for [5, 7] using the dataset
 result = k_nearest_neighbors(dataset, new_features)
 print('New feature belongs to ' + result[0] + ' group')
 
+#Plotting it will make it clear, yep
 [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
 
 plt.scatter(new_features[0], new_features[1], s=100, color=result)
--- 8_classification_k_nearest_neighbors.py
+++ 8_classification_k_nearest_neighbors.py
@@ -2,24 +2,42 @@ import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
 
+#Read the breast cancer data using pandas
+#Basics:
+#Input: Various parameters of a lump on a breast like lump size, texture, etc (score out of 10)
+#Output: Is the tumour malignant or benign (2 or 4)
 df = pd.read_csv('breast-cancer-wisconsin.data.txt', skipinitialspace=True)
+
+#Replace every empty field with a -99999
 df.replace('?', -99999, inplace=True)
+
+#Remove the id column as it is irrelevant
 df.drop(['id'], 1, inplace=True)
 
+#Build input: all the columns except the 'class' column
 X = np.array(df.drop(['class'], 1))
+#Build output: only the 'class' column
 y = np.array(df['class'])
 
+#Get data for training and testing randomly
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
+#Create a K Nearest Neighbors classifiers
 clf = neighbors.KNeighborsClassifier()
+
+#Train the algorithm by training data
 clf.fit(X_train, y_train)
 
+#Run the algorithm on test data and get the accuracy
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
 
+#Calculate decision for
 example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 1, 2, 2, 3, 2, 1]])
 
 #For fixing the deprecation warning            in our case = 2
 example_measures = example_measures.reshape(len(example_measures), -1)
+
+#Predict the result for the examples given in example_measures(both of them)
 prediction = clf.predict(example_measures)
 print(prediction)
